865944637aab66e39e7de34a6796d0dbfb614478
Chu Phong <chucamphong1999@gmail.com>
2020-05-16 18:44:12 +0700

    Optimize & Reformat: Tối ưu hàm OnGetAsync và OnPostAsync
    Cập nhật:
    - Kiểm tra người dùng nếu đã đăng nhập thì không được truy cập vào trang
    - Tối ưu lại ReturnUrl
    - Tối ưu lại việc import các thư viện
    - Thêm chú thích
    
    Xóa bỏ:
    - Loại bỏ đăng nhập bên thứ 3
    - Loại bỏ ghi log

--- CPMusic/CPMusic/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CPMusic/CPMusic/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,41 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Reflection;
-using System.Text.Encodings.Web;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
-using CPMusic.Resources;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace CPMusic.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LoginModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager, 
-            ILogger<LoginModel> logger,
-            UserManager<IdentityUser> userManager)
+        public LoginModel(SignInManager<IdentityUser> signInManager)
         {
-            _userManager = userManager;
             _signInManager = signInManager;
-            _logger = logger;
         }
 
         [BindProperty]
         public InputModel Input { get; set; }
-
-        public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
         public string ReturnUrl { get; set; }
 
@@ -57,59 +42,68 @@ namespace CPMusic.Areas.Identity.Pages.Account
             public bool RememberMe { get; set; }
         }
 
-        public async Task<IActionResult> OnGetAsync(string returnUrl = null)
+        /// <summary>
+        /// Trang đăng nhập
+        /// </summary>
+        /// ReSharper disable once UnusedMember.Global
+        public async Task<IActionResult> OnGetAsync(string? returnUrl)
         {
+            // Nếu đã đăng nhập rồi thì chuyển về trang chủ
             if (_signInManager.IsSignedIn(User))
             {
                 return LocalRedirect("~/");
             }
-            
+
             if (!string.IsNullOrEmpty(ErrorMessage))
             {
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("~/");
-
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-
-            ReturnUrl = returnUrl;
+            ReturnUrl = returnUrl ?? Url.Content("~/");
 
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        /// <summary>
+        /// Xử lý đăng nhập
+        /// </summary>
+        /// ReSharper disable once UnusedMember.Global
+        public async Task<IActionResult> OnPostAsync(string? returnUrl)
         {
-            returnUrl ??= Url.Content("~/");
+            ReturnUrl = returnUrl ?? Url.Content("~/");
 
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
-                if (result.Succeeded)
-                {
-                    return LocalRedirect(returnUrl);
-                }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-                }
-                if (result.IsLockedOut)
-                {
-                    return RedirectToPage("./Lockout");
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Tài khoản hoặc mật khẩu không đúng.");
-                    return Page();
-                }
+                return Page();
             }
 
-            // If we got this far, something failed, redisplay form
+            // Muốn bật tính năng khóa tài khoản khi đăng nhập sai quá nhiều lần thì đặt lockoutOnFailure thành true
+            SignInResult result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, false);
+
+            // Đăng nhập thành công
+            if (result.Succeeded)
+            {
+                return LocalRedirect(ReturnUrl);
+            }
+
+            // Chuyển sang trang LoginWith2fa vì yêu cầu xác thực hai bước
+            if (result.RequiresTwoFactor)
+            {
+                return RedirectToPage("./LoginWith2fa", new { ReturnUrl, Input.RememberMe });
+            }
+
+            // Tài khoản đã bị khóa
+            if (result.IsLockedOut)
+            {
+                return RedirectToPage("./Lockout");
+            }
+
+            // Tài khoản hoặc mật khẩu không đúng
+            ModelState.AddModelError(string.Empty, "Tài khoản hoặc mật khẩu không đúng.");
+
             return Page();
         }
     }
