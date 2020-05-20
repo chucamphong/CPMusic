b7942eaacc6bb13a5b4f781ce4120497ff771dbb
Chu Phong <chucamphong1999@gmail.com>
2020-05-20 18:29:59 +0700

    Fix: Sửa lỗi asp-area không hoạt động khi xài chung với asp-controller, asp-action

--- CPMusic/CPMusic/Startup.cs
+++ CPMusic/CPMusic/Startup.cs
@@ -75,12 +75,12 @@ namespace CPMusic
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-                endpoints.MapControllerRoute(
                     name: "areas",
                     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
 
                 endpoints.MapRazorPages();
             });
