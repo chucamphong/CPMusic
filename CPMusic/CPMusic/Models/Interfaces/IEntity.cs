714af10964ed538183068329d55ca72bd859e234
Chu Phong <chucamphong1999@gmail.com>
2020-05-21 23:08:02 +0700

    Fix: Thêm trường Name

--- CPMusic/CPMusic/Models/Interfaces/IEntity.cs
+++ CPMusic/CPMusic/Models/Interfaces/IEntity.cs
@@ -5,6 +5,8 @@ namespace CPMusic.Models.Interfaces
     public interface IEntity
     {
         public int Id { get; set; }
+        
+        public string Name { get; set; }
 
         public DateTime CreatedAt { get; set; }
     }
