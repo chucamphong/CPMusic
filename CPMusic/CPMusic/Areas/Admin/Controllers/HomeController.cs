249296692e50abfdc5fdf55db4c66419eacfe515
Chu Phong <chucamphong1999@gmail.com>
2020-05-21 23:09:29 +0700

    Fix: Hiển thị chênh lệch % số bài hát của tuần này với tuần trước
    - Tối ưu lại phần hiển thị thống kê

--- CPMusic/CPMusic/Areas/Admin/Controllers/HomeController.cs
+++ CPMusic/CPMusic/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using CPMusic.Data;
+using CPMusic.Models.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,17 +13,51 @@ namespace CPMusic.Areas.Admin.Controllers
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _context;
-        
+
+        public class StaticialStats
+        {
+            public string Name { get; set; }
+            public int Total { get; set; }
+            public double Percent { get; set; }
+        }
+
         public HomeController(ApplicationDbContext context)
         {
             _context = context;
         }
-        
+
+        private (int, double) EntityGrowth(IQueryable<IEntity> entity)
+        {
+            double growth = 0;
+            int month = DateTime.Now.Month;
+            int totalNumberOfEntityPrevMonth = entity.Count(col => col.CreatedAt.Month == month - 1);
+            int totalNumberOfEntityThisMonth = entity.Count(col => col.CreatedAt.Month == month);
+            int totalNumberOfEntity = entity.Count();
+
+            if (totalNumberOfEntityThisMonth != 0 || totalNumberOfEntityPrevMonth != 0)
+            {
+                growth = (totalNumberOfEntityThisMonth - totalNumberOfEntityPrevMonth) /
+                         (double) totalNumberOfEntityPrevMonth;
+            }
+
+            return (totalNumberOfEntity, growth);
+        }
+
         // GET
         public IActionResult Index()
         {
-            ViewData["TotalSong"] = _context.Songs.Count().ToString();
-            return View();
+            List<StaticialStats> staticialStatses = new List<StaticialStats>();
+            
+            var (totalNumberOfSongsThisMonth, songGrowth) = EntityGrowth(_context.Songs);
+
+            staticialStatses.Add(new StaticialStats
+            {
+                Name = "Bài hát",
+                Total = totalNumberOfSongsThisMonth,
+                Percent = songGrowth
+            });
+
+            return View(staticialStatses);
         }
     }
 }
