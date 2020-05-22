654c4a3ff0222f4e9a6d43cb0014a98191319aed
Chu Phong <chucamphong1999@gmail.com>
2020-05-22 15:39:39 +0700

    Add & Fix: Hiển thị số lượng thành viên, sửa lỗi
    - Sửa lỗi % chênh lệch thống kê bị hiện ∞

--- CPMusic/CPMusic/Areas/Admin/Controllers/HomeController.cs
+++ CPMusic/CPMusic/Areas/Admin/Controllers/HomeController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using CPMusic.Data;
+using CPMusic.Models;
 using CPMusic.Models.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CPMusic.Areas.Admin.Controllers
@@ -13,17 +15,19 @@ namespace CPMusic.Areas.Admin.Controllers
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
 
         public class StaticialStats
         {
-            public string Name { get; set; }
+            public string Name { get; set; } = null!;
             public int Total { get; set; }
             public double Percent { get; set; }
         }
 
-        public HomeController(ApplicationDbContext context)
+        public HomeController(ApplicationDbContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         private (int, double) EntityGrowth(IQueryable<IEntity> entity)
@@ -34,10 +38,15 @@ namespace CPMusic.Areas.Admin.Controllers
             int totalNumberOfEntityThisMonth = entity.Count(col => col.CreatedAt.Month == month);
             int totalNumberOfEntity = entity.Count();
 
-            if (totalNumberOfEntityThisMonth != 0 || totalNumberOfEntityPrevMonth != 0)
+            if (totalNumberOfEntityPrevMonth == 0)
+            {
+                growth = totalNumberOfEntityThisMonth;
+            }
+
+            if (totalNumberOfEntityPrevMonth != 0)
             {
                 growth = (totalNumberOfEntityThisMonth - totalNumberOfEntityPrevMonth) /
-                         (double) totalNumberOfEntityPrevMonth;
+                         (double)totalNumberOfEntityPrevMonth;
             }
 
             return (totalNumberOfEntity, growth);
@@ -47,14 +56,22 @@ namespace CPMusic.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<StaticialStats> staticialStatses = new List<StaticialStats>();
-            
+
             var (totalNumberOfSongsThisMonth, songGrowth) = EntityGrowth(_context.Songs);
+            var (totalNumberOfUsersThisMonth, userGrowth) = EntityGrowth(_userManager.Users);
 
             staticialStatses.Add(new StaticialStats
             {
                 Name = "Bài hát",
                 Total = totalNumberOfSongsThisMonth,
                 Percent = songGrowth
+            });
+
+            staticialStatses.Add(new StaticialStats
+            {
+                Name = "Người dùng",
+                Total = totalNumberOfUsersThisMonth,
+                Percent = userGrowth
             });
 
             return View(staticialStatses);
