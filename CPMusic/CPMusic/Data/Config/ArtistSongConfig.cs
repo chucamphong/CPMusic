45ea6b40782785afeb0df5bb9b1922089a96f881
Chu Phong <chucamphong1999@gmail.com>
2020-05-22 18:36:13 +0700

    Rephrase: Thêm chú thích tạo quan hệ n - n bảng nghệ sĩ và thể loại

--- CPMusic/CPMusic/Data/Config/ArtistSongConfig.cs
+++ CPMusic/CPMusic/Data/Config/ArtistSongConfig.cs
@@ -11,16 +11,19 @@ namespace CPMusic.Data.Config
         {
             builder.HasKey(col => new { col.ArtistId, col.SongId });
 
-            // Tạo quan hệ n - n giữa bảng Nghệ sĩ - Thể loại
+            #region Tạo quan hệ n - n giữa bảng Nghệ sĩ - Thể loại
+
             builder.HasOne(col => col.Artist)
                    .WithMany(col => col.ArtistSongs)
                    .HasForeignKey(col => col.ArtistId);
-            
+
             builder
                 .HasOne(col => col.Song)
                 .WithMany(col => col.ArtistSongs)
                 .HasForeignKey(col => col.SongId);
-            
+
+            #endregion
+
             builder.HasData(new ArtistSong
             {
                 ArtistId = Guid.Parse("09C1BFE7-D884-4484-8F66-8225536214C9"),
