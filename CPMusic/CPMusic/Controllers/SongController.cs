3cccfa29ed5040859d6291be8c04be59f26db416
ChuPhong <chucamphong1999@gmail.com>
2020-05-31 16:54:08 +0700

    Add: Giao diện
    - Giao diện Argon Premium
    - Trang mới phát hành

---
+++ CPMusic/CPMusic/Controllers/SongController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CPMusic.Controllers
+{
+    [Route("bai-hat")]
+    public class SongController : Controller
+    {
+        [Route("moi-phat-hanh")]
+        public IActionResult NewSongsReleased()
+        {
+            return View();
+        }
+    }
+}
