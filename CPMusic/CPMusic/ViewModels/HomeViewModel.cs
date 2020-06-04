5d5051c0018065d0c6ccbf2535f8cab10cdf7c73
ChuPhong <chucamphong1999@gmail.com>
2020-06-04 18:30:36 +0700

    Refactor: Sử dụng ViewModel thay thế

---
+++ CPMusic/CPMusic/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CPMusic.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<SongViewModel> RandomSongs { get; set; } = null!;
+        
+        public IEnumerable<SongViewModel> NewSongsReleased { get; set; } = null!;
+
+        public IEnumerable<CategoryViewModel> Categories { get; set; } = null!;
+    }
+}
