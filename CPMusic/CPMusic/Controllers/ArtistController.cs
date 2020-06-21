f4294a9b12ec35479037218a863104c0a76cf3d0
ChuPhong <chucamphong1999@gmail.com>
2020-06-21 12:04:08 +0700

    Add: Trang nghệ sĩ (Chưa hoàn thiện)

---
+++ CPMusic/CPMusic/Controllers/ArtistController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using CPMusic.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CPMusic.Controllers
+{
+    public class ArtistController : Controller
+    {
+        private readonly IArtistRepository _artistRepository;
+
+        public ArtistController(IArtistRepository artistRepository)
+        {
+            _artistRepository = artistRepository;
+        }
+        
+        /// <summary>
+        /// GET: /nghe-si/{id}
+        /// Trang nghệ sĩ
+        /// </summary>
+        /// TODO: Làm trang nghệ sĩ
+        [Route("nghe-si/{id}")]
+        public async Task<IActionResult> Profile(Guid? id)
+        {
+            if (id is null) return NotFound();
+
+            var artist = await _artistRepository.GetByIdAsync((Guid) id);
+
+            if (artist is null) return NotFound();
+
+            return View(artist);
+        }
+    }
+}
